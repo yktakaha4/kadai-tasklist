688b628750ec9229e9f0af3f872865f8ed370179
yk.takaha4 <yk.takaha4@gmail.com>
2018-08-11 19:35:45 +0000

    add status column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :init_definitions
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -24,12 +26,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -40,7 +39,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -49,9 +47,17 @@ class TasksController < ApplicationController
   
   private 
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def init_definitions
+    @status_list = {'ready' => '着手可', 'doing' => '着手中', 'done' => '完了', 'onhold' => '保留'}
   end
   
 end
