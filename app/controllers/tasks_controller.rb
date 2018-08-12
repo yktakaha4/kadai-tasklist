ad77ab4175f40439a72a2a33fdcbf7fa7636d2f5
yk.takaha4 <yk.takaha4@gmail.com>
2018-08-12 19:47:37 +0000

    customize layouts with bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   before_action :init_definitions
 
   def index
-    @tasks = Task.all
+    @tasks = Task.all.page(params[:page]).per(5)
   end
 
   def show
